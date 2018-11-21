f4c77ff6ca6279e58b85fe0ce77fd43ed0b23feb
RtyXmd <573850993@qq.com>
2018-11-21 15:32:20 +0800

    增加组件loading加载文档

---
+++ docs/.vuepress/components/example/loading.vue
@@ -0,0 +1,45 @@
+<template>
+  <div>
+    <div style="margin:20px 0">
+      <rty-button :text="'全屏loading'" :type="'primary'" @click="displayLoad1"></rty-button>
+      <rty-loading :visible="showLoading1"></rty-loading>
+    </div>
+    <div>
+      <rty-button :text="'局部loading'" :type="'primary'" @click="displayLoad2"></rty-button>
+      <div style="position:relative;width:500px;height:200px;border:1px solid #333;">
+        <rty-loading :visible="showLoading2" :position="'absolute'"></rty-loading>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import rtyButton from "../src/rtyButton";
+import rtyLoading from "../src/rtyLoading";
+export default {
+  components: {
+    rtyButton,
+    rtyLoading
+  },
+  data() {
+    return {
+      showLoading1: false,
+      showLoading2: false,
+    }
+  },
+  methods: {
+    displayLoad1() {
+      this.showLoading1 = true
+      setTimeout(() => {
+        this.showLoading1 = false
+      },2000)
+    },
+    displayLoad2() {
+      this.showLoading2 = true
+      setTimeout(() => {
+        this.showLoading2 = false
+      },2000)
+    }
+  },
+};
+</script>
---
+++ docs/.vuepress/components/src/rtyLoading.vue
@@ -0,0 +1,53 @@
+<template>
+  <div class="rty-loading flex-center-center" :style="{position:position}" v-show="visible">
+    <div class="load-effect"></div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    position: {
+      type: String,
+      default: "fixed"
+    },
+    visible: {
+      type: Boolean,
+      default: false
+    }
+  }
+};
+</script>
+
+<style scoped>
+.flex-center-center {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+.rty-loading {
+  top: 0;
+  left: 0;
+  right: 0;
+  bottom: 0;
+  z-index: 2018;
+  background: rgba(0, 0, 0, 0.05);
+}
+.load-effect {
+  width: 40px;
+  height: 40px;
+  border: 2px solid;
+  border-color: #409eff #409eff transparent;
+  border-radius: 50%;
+  box-sizing: border-box;
+  animation: loading 1s linear infinite;
+}
+@keyframes loading {
+  0% {
+    transform: rotate(0deg);
+  }
+  100% {
+    transform: rotate(360deg);
+  }
+}
+</style>
