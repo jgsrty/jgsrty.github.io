80361ffc386c004fcf40c868b030401a1d52c3a0
RtyXmd <573850993@qq.com>
2018-11-21 16:19:01 +0800

    增加rtyPop气泡提示组件文档

---
+++ docs/.vuepress/components/example/pop.vue
@@ -0,0 +1,25 @@
+<template>
+  <div style="margin-top:10px">
+    <rty-button :text="'打开气泡提示'" :type="'primary'" @click="showPop = true"></rty-button>
+    <rty-pop :visible.sync="showPop" :fade="2500" :text="'信息已提交，请耐心等待'"></rty-pop>
+  </div>
+</template>
+
+<script>
+import rtyButton from "../src/rtyButton";
+import rtyPop from "../src/rtyPop";
+export default {
+  components: {
+    rtyButton,
+    rtyPop
+  },
+  data() {
+    return {
+      showPop: false
+    };
+  }
+};
+</script>
+
+<style scoped>
+</style>
---
+++ docs/.vuepress/components/src/rtyPop.vue
@@ -0,0 +1,59 @@
+<template>
+  <transition name="fade">
+    <div class="rty-pop flex-center-center" v-show="visible">
+      <div class="pop">{{text}}</div>
+    </div>
+  </transition>
+</template>
+
+<script>
+export default {
+  props: {
+    visible: {
+      type: Boolean,
+      default: false
+    },
+    text: {
+      type: String,
+      default: "气泡提示"
+    },
+    fade: {
+      type: Number,
+      default: 1500
+    }
+  },
+  watch: {
+    visible(val) {
+      if (val) {
+        setTimeout(() => {
+          this.$emit("update:visible", false);
+        },this.fade);
+      }
+    }
+  }
+};
+</script>
+
+<style scoped lang="scss">
+/* flex */
+.flex-center-center {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+.rty-pop {
+  position: fixed;
+  top: 0;
+  bottom: 0;
+  left: 0;
+  right: 0;
+  z-index:2018;
+  background: rgba(255, 255, 255, 0);
+  .pop {
+    color: #fff;
+    background: rgba(0, 0, 0, 0.8);
+    padding: 10px;
+    border-radius: 6px;
+  }
+}
+</style>
