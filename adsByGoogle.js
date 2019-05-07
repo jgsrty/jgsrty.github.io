a33a368747957c66784b044cc0201edc7c5486dd
jgsrty <573850993@qq.com>
2019-05-07 15:09:12 +0800

    1.add google Ad Sense 2.change deploy.sh (not build & add google Ad Sense to entry file before npm run sh)

---
+++ adsByGoogle.js
@@ -0,0 +1,9 @@
+/*
+<script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
+<script>
+  (adsbygoogle = window.adsbygoogle || []).push({
+    google_ad_client: "ca-pub-4354974357850752",
+    enable_page_level_ads: true
+  });
+</script>
+*/
