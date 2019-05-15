e10756f2055ef98dafc9a7459abba35cd1cc4142
liucunzhou <liucunzhou@163.com>
2019-05-15 18:16:56 +0800

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -17,7 +17,7 @@ git add -A
 git commit -m 'deploy'
 
 # 如果你想要部署到 https://<USERNAME>.github.io
-git push -f git@github.com:jgsrty/jgsrty.github.io.git master
+git push -f git@github.com:liucunzhou/liucunzhou.github.io.git master
 
 # 如果你想要部署到 https://<USERNAME>.github.io/<REPO>
 # git push -f git@github.com:<USERNAME>/<REPO>.git master:gh-pages
