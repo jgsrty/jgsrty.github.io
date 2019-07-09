b1123c5d16a3809090f14fa0abc6fc0494c7ae78
RtyXmd <573850993@qq.com>
2019-07-09 15:05:33 +0800

    1.add component rtyAudio 2.Working to live or living to work --add original--

---
+++ docs/.vuepress/components/src/rtyAudio.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="position">
+    <audio
+      id="audio"
+      loop
+      controls=""
+      controlsList="nodownload"
+      oncontextmenu="return false"
+      preload=""
+    >
+      <source
+        id="mp3"
+        :src="src"
+      >
+    </audio>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "rtyAudio",
+  props: {
+    src: "",
+  },
+};
+</script>
+
+<style scoped lang="scss">
+.position {
+  width: 100%;
+  position: sticky;
+  bottom: 0;
+}
+</style>
