f943582372efa2e0caa66e94b245171c9daf2e70
RtyXmd <573850993@qq.com>
2019-08-21 17:18:10 +0800

    add gitee deploy

---
+++ deployGitee.sh
@@ -0,0 +1,23 @@
+# 终止一个错误
+set -e
+
+# 构建
+npm run build
+
+# 进入生成的构建文件夹
+cd docs/.vuepress/dist
+
+# 如果你是要部署到自定义域名
+# echo 'www.example.com' > CNAME
+
+git init
+git add -A
+git commit -m 'https://rtyxmd.gitee.io'
+
+# 如果你想要部署到 https://<USERNAME>.github.io
+git push -f git@gitee.com:RtyXmd/rtyxmd.git master
+
+# 如果你想要部署到 https://<USERNAME>.github.io/<REPO>
+# git push -f git@github.com:<USERNAME>/<REPO>.git master:gh-pages
+
+cd -
