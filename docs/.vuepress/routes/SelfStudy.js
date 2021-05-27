2563018cb6fb844e5f646314c8d1bdeb43190006
RtyXmd <573850993@qq.com>
2021-05-27 23:20:54 +0800

    add Ancient Chinese Literature Two structure

--- docs/.vuepress/routes/SelfStudy.js
+++ docs/.vuepress/routes/SelfStudy.js
@@ -4,6 +4,9 @@ module.exports = [
   {
     title: "汉语言文学",
     collapsable: false,
-    children: ["SelfStudy/ChineseLiterature/宋词研究"],
+    children: [
+      "SelfStudy/ChineseLiterature/中国古代文学史二",
+      "SelfStudy/ChineseLiterature/宋词研究",
+    ],
   },
 ];
