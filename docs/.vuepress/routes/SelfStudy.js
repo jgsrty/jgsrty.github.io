7b153daa16330332b02dd574c56a83c3754416f6
RtyXmd <573850993@qq.com>
2021-07-13 17:01:46 +0800

    外国文学史 init

--- docs/.vuepress/routes/SelfStudy.js
+++ docs/.vuepress/routes/SelfStudy.js
@@ -5,8 +5,9 @@ module.exports = [
     title: "汉语言文学",
     collapsable: false,
     children: [
-      "SelfStudy/ChineseLiterature/语言学概论",
+      "SelfStudy/ChineseLiterature/外国文学史",
       "SelfStudy/ChineseLiterature/中国古代文学史二",
+      "SelfStudy/ChineseLiterature/语言学概论",
       "SelfStudy/ChineseLiterature/宋词研究",
     ],
   },
