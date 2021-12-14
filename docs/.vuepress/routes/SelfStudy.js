8531ac5b354ca4f2f3703b0b29f5373810c5ee37
RtyXmd <rtyxmd753951@gmail.com>
2021-12-14 16:13:15 +0800

    add new course

--- docs/.vuepress/routes/SelfStudy.js
+++ docs/.vuepress/routes/SelfStudy.js
@@ -5,6 +5,9 @@ module.exports = [
     title: "汉语言文学",
     collapsable: false,
     children: [
+      "SelfStudy/ChineseLiterature/美学",
+      "SelfStudy/ChineseLiterature/中国现代文学史",
+      "SelfStudy/ChineseLiterature/中国现代文学作品选",
       "SelfStudy/ChineseLiterature/外国文学史",
       "SelfStudy/ChineseLiterature/中国古代文学史二",
       "SelfStudy/ChineseLiterature/鲁迅研究",
