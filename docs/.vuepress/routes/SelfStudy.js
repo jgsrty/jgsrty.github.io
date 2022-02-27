5c7896332d598df27c2b840eb1e7897ce1751cc8
RtyXmd <573850993@qq.com>
2022-02-27 20:33:47 +0800

    美学update

--- docs/.vuepress/routes/SelfStudy.js
+++ docs/.vuepress/routes/SelfStudy.js
@@ -5,7 +5,12 @@ module.exports = [
     title: "汉语言文学",
     collapsable: false,
     children: [
-      "SelfStudy/ChineseLiterature/美学",
+      "SelfStudy/ChineseLiterature/美学-绪论",
+      "SelfStudy/ChineseLiterature/美学-审美活动论",
+      "SelfStudy/ChineseLiterature/美学-审美形态论",
+      "SelfStudy/ChineseLiterature/美学-审美经验论",
+      "SelfStudy/ChineseLiterature/美学-艺术论",
+      "SelfStudy/ChineseLiterature/美学-审美教育论",
       "SelfStudy/ChineseLiterature/中国现代文学史",
       "SelfStudy/ChineseLiterature/中国现代文学作品选",
       "SelfStudy/ChineseLiterature/外国文学史",
