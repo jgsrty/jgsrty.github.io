5b7dea94c1faa0c952f58ac12ec3cd45269c7cc6
RtyXmd <rongtianyang@inin88.com>
2022-08-01 09:34:18 +0800

    'fix'

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -15,7 +15,8 @@ module.exports = {
   description: "Welcome to my docs",
   plugins: [
     docsearchPlugin({
-      apiKey: 'd69147481e9bbd6bb55e9793a8d0df53',
+      appId: "PFAMZDC9Z4",
+      apiKey: 'b528d6541463420ac954f7fca1565180',
       indexName: 'jgsrty',
     }),
   ],
