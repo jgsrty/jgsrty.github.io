d1477dbc267df025ab5f4721b74a84e95eeebf23
inin88_rongtianyang <rongtianyang@inin88.com>
2023-11-10 18:08:04 +0800

    star

---
+++ docs/.vuepress/routes/test.js
@@ -0,0 +1,14 @@
+module.exports = [
+  {
+    text: "英语原著",
+    collapsible: true,
+    children: [
+      "/english/test/introduction",
+      "/english/test/⭐️",
+      "/english/test/⭐️⭐️",
+      "/english/test/⭐️⭐️⭐️",
+      "/english/test/⭐️⭐️⭐️⭐️",
+      "/english/test/⭐️⭐️⭐️⭐️⭐️",
+    ],
+  },
+];
